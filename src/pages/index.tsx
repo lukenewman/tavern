4a9ddbc7002b3ee5acea37d41bed6eca012929ec
jonandgon <jag73155@usc.edu>
2023-09-23 15:22:13 -0400

    add the db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useAccount } from 'wagmi';
-import Link from 'next/link'
-import Image from 'next/image';
+import Link from 'next/link';
+import { Image } from 'next/image';
 import tavern from './tavern.jpg';
 
 export default function Home() {
